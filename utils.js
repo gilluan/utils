6c610a2b2155384f408e418a725f409bb6ef6cef
Gilluan <gilluan@hotmail.com.br>
2016-05-20 18:51:03 -0300

    utils.js
    Projetos para inclusao de utilitários

---
+++ utils.js
@@ -0,0 +1,76 @@
+//Algoritimo de edição e exclusão
+// Será necessário um refactory em todos os requerimentos
+// Específico para uso da dpf
+
+
+(function() {
+  var piloto = {
+    id: null,
+    nome: null,
+    valores: [
+      {nomeValores: null}
+    ]
+  };
+  
+  
+  
+  var json = [
+    {nomeCampo: 'id', valor: 1},
+    {nomeCampo: 'nome',valor: 'gilluan'},
+    {nomeCampo: 'nomeValores',valor: 'abc11111'},
+    {nomeCampo: 'nomeValores',valor: 'asdfafsd'}
+  ];
+  
+  function contarRepeticoes(obj) {
+    _.filter();
+  }
+  
+  contarRepeticoes(json);
+  
+  function has(obj, str) {
+    _.forEach(obj, function(value, key) {
+      if(_.isObject(value)) {
+        has(value, "" + key + str);
+      } else {
+        console.log(str + ":" + value);
+      }
+    });
+  }
+  
+  has(piloto);
+  
+  function preencherObjTmp(objtmp, item) {
+    
+    _.forEach(item, function (value, key) {
+      
+        console.log('valor: ' + value + "| key: " + key);
+     // has(objtmp, value);
+        if(_.has(objtmp, value)) {
+          console.log(typeof objtmp[value]);
+        
+          objtmp[value] = item.valor;
+      
+        }  
+    
+      });
+  }
+  
+  function converterMapa(obj) {
+    
+    
+    var objtmp = _.clone(piloto);
+    
+    _.map(obj, function(item, i) {
+      
+      
+
+        preencherObjTmp(objtmp, item);  
+
+      
+    });
+    console.log('Piloto' + JSON.stringify(piloto));
+   // console.log('ObjTmp' + JSON.stringify(objtmp));
+
+  }
+  //converterMapa(json);
+})();
